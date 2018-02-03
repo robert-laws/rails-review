bf179d1c7e259783a918e9a9ccc2d6f457e22fde
Robert Laws <roblaws@me.com>
2018-02-03 20:25:19 +0300

    basic app setup, prepare for deploy to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Country.destroy_all
+
+Country.create(name: "Norway", capital: "Oslo")
+Country.create(name: "Sweden", capital: "Stockholm")
+Country.create(name: "Denmark", capital: "Copenhagen")
+Country.create(name: "Finland", capital: "Helsinki")
