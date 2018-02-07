6d6ae9905698263c279771b65da1cbb5975133ae
robert-laws <roblaws@me.com>
2018-02-07 09:51:48 +0300

    update model relationships and seed - create cities belonging to a country

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,19 +1,32 @@
 class Country < ApplicationRecord
+  # relationships
+  has_many :cities
+
   # validations
   validates :name, presence: true
   validates :capital, presence: true
 
-  # Model scope without parameters, can be interchanged with class methods
+  # custom scope without parameters, can be interchanged with class methods
   scope :finnish, -> { where("name == ?", "Finland") }
-
   scope :big_population, -> { where("population > ?", 5500000)}
   scope :late_independence, -> { where("foundation_year > ?", 1800)}
 
+  # model class method
   def self.country_name(name)
     where("name == ?", name)
   end
 
+  # model instance method
   def country_summary
     self.name + " - " + self.capital
+  end
+
+  # callback
+  after_validation :check_error_message
+
+  private
+
+  def check_error_message
+    puts "remember to check error message..."
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,20 @@
 Country.destroy_all
 
-Country.create(name: "Norway", capital: "Oslo", population: 5233000, foundation_year: 1905)
-Country.create(name: "Sweden", capital: "Stockholm", population: 9903000, foundation_year: 1523)
-Country.create(name: "Denmark", capital: "Copenhagen", population: 5731000, foundation_year: 1814)
-Country.create(name: "Finland", capital: "Helsinki", population: 5495000, foundation_year: 1917)
+@norway = Country.create(name: "Norway", capital: "Oslo", population: 5233000, foundation_year: 1905)
+@sweden = Country.create(name: "Sweden", capital: "Stockholm", population: 9903000, foundation_year: 1523)
+@denmark = Country.create(name: "Denmark", capital: "Copenhagen", population: 5731000, foundation_year: 1814)
+@finland = Country.create(name: "Finland", capital: "Helsinki", population: 5495000, foundation_year: 1917)
+
+City.create(name: "Oslo", population: 755000, country: @norway)
+City.create(name: "Bergen", population: 264000, country: @norway)
+City.create(name: "Trondheim", population: 142000, country: @norway)
+
+City.create(name: "Stockholm", population: 984000, country: @sweden)
+City.create(name: "Malmo", population: 341000, country: @sweden)
+City.create(name: "Gotenborg", population: 486000, country: @sweden)
+
+City.create(name: "Copenhagen", population: 1240000, country: @denmark)
+City.create(name: "Aarhus", population: 212000, country: @denmark)
+
+City.create(name: "Helsinki", population: 398000, country: @finland)
+City.create(name: "Turku", population: 179000, country: @finland)
