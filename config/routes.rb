30e7610a6e5cff27b0e4244438d2206a150d445c
Robert Laws <roblaws@me.com>
2018-02-18 20:40:24 +0300

    file cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  
+
   # resources :countries, only: [:index, :show, :new]
 
   resources :cities, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   get '/countries', to: 'countries#index'
   post '/countries', to: 'countries#create'
-  
+
   get '/countries/new', to: 'countries#new', as: 'new_country'
 
   get '/countries/:id', to: 'countries#show', as: 'country'
   get '/countries/:id/edit', to: 'countries#edit', as: 'edit_country'
   patch '/countries/:id', to: 'countries#update'
-  
+
   get '/countries/:id/change_membership', to: 'countries#change_membership', as: 'change_membership'
 
   # Static Controller & View
@@ -22,6 +22,6 @@ Rails.application.routes.draw do
   # Contact Form
   get '/contact', to: 'static#contact'
   post '/contact', to: 'static#comment'
-  
+
   root 'countries#index'
 end
